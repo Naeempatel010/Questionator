d4ac8e400661f6ecac263d4445a86b7c4bb6c726
Naeempatel010 <naeempatel010@gmail.com>
2019-07-02 23:20:06 +0530

    code

---
+++ HTML/caption.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<title>EndGam - Gaming Magazine Template</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="EndGam Gaming Magazine Template">
+	<meta name="keywords" content="endGam,gGaming, magazine, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->
+	<link href="img/favicon.ico" rel="shortcut icon"/>
+
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/slicknav.min.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+
+	<!-- Main Stylesheets -->
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- Header section -->
+	<header class="header-section">
+		<div class="header-warp">
+			<div class="header-bar-warp d-flex">
+				<!-- site logo -->
+				<a href="home.html" class="site-logo">
+					<img src="./img/logo.png" alt="">
+				</a>
+				<nav class="top-nav-area w-100">
+					<!-- Menu -->
+					<ul class="main-menu primary-menu">
+						<li><a href="home.html">Home</a></li>
+						<li><a href="games.html">Games</a>
+							<ul class="sub-menu">
+								<li><a href="game-single.html">Game Singel</a></li>
+							</ul>
+						</li>
+						<li><a href="review.html">Reviews</a></li>
+						<li><a href="blog.html">News</a></li>
+						<li><a href="contact.html">Contact</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+	<!-- Header section end -->
+
+	<!-- Page top section -->
+	
+	<section class="page-top-section set-bg" data-setbg="img/page-top-bg/1.jpg">
+		<div class="page-info">
+			<h2>Captioning Images</h2>
+			<div class="site-breadcrumb">
+				<a href=""></a>  				
+				<span></span>
+			</div>
+		</div>
+	</section>
+
+	<!-- Page top end-->
+
+
+	<!-- Games section -->
+	<section class="games-single-page">
+		<div class="container">
+			<div class="game-single-preview">
+				<img src="" alt="" id="caption-img2">
+			</div>		
+			<div class="row">
+				<div style="text-align:center">
+					<a href="#" class="site-btn" style="" onclick="captiongen()">Generate Caption<img src="img/icons/double-arrow.png" alt="#">
+
+					</a>
+				</div>
+			<div class="col-xl-3 col-lg-4 col-md-5 sidebar game-page-sideber">
+					<div id="stickySidebar">
+						<div class="widget-item">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Games end-->
+
+	<section class="game-author-section">
+		<div class="container">
+			<div class="game-author-pic set-bg" data-setbg="img/author.jpg"></div>
+			<div class="game-author-info">
+				<h4>Website by: M-vision</h4>
+				<p>Questionator.ai is a advance question generating bot which can generate images for images as well as text, the first of its kind.It used a number of machine learning and semantic approaches to achieve these results</p>
+			</div>
+		</div>
+	</section>
+
+
+	<!-- Newsletter section end -->
+
+
+	<!-- Footer section -->
+	<footer class="footer-section">
+		<div class="container">
+			<div class="copyright"><a href="">M-vision</a> 2019 @ All rights reserved</div>
+		</div>
+	</footer>
+	<!-- Footer section end -->
+
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.sticky-sidebar.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/main.js"></script>
+	<script src="js/caption.js"></script>
+	</body>
+</html>
